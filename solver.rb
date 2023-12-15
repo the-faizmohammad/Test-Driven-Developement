0f33ba7d8655974fefd8322b866c1e99f8c62e13
user-faiz <email2faizm@gmail.com>
2023-12-16 00:58:10 +0530

    add fizzbuzz metod

--- solver.rb
+++ solver.rb
@@ -9,3 +9,12 @@ end
 def reverse(word)
   word.reverse
 end
+
+def fizzbuzz(n)
+    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    n.to_s
+  end
+end
