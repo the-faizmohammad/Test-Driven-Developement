a7744808ff49a8748cadc0e849580465245a4088
user-faiz <email2faizm@gmail.com>
2023-12-16 00:57:07 +0530

    add test for fizzbuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,25 @@
+require_relative './solver'
+
+RSpec.describe Solver do
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for multiples of 3' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for multiples of 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for other cases' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
+end
